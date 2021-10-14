ca0f70e5f931b21ff9fd8cf2566da2674c07aa69
Alexander Sharma <alexander.sharma@gmail.com>
2021-10-14 12:11:17 -0700

    Starwars movie Crawl scenes from youtube: https://www.youtube.com/watch?v=Mrztcf1B-0k&ab_channel=AMBPineapple

---
+++ src/pages/Home.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Meta from 'react-helmet';
+
+import meta from '../util/meta/home-page';
+import { fetchSearchesIfNeeded } from '../actions/search';
+
+import Header from '../components/Header';
+import Searchbox from '../components/Searchbox';
+import Suggestions from '../components/Suggestions';
+import MyInfo from '../components/MyInfo';
+
+export class Home extends Component {
+  static propTypes = {
+    searchString: PropTypes.string,
+    dispatch: PropTypes.func,
+    searchesByString: PropTypes.object,
+    films: PropTypes.object,
+    characters: PropTypes.object
+  }
+
+  static getMeta() {
+    return meta;
+  }
+
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    const { dispatch, searchString } = this.props;
+    dispatch(fetchSearchesIfNeeded(searchString));
+  }
+
+  handleChange(searchString) {
+    this.props.dispatch(fetchSearchesIfNeeded(searchString));
+  }
+
+  render() {
+    const { searchString, searchesByString, films, characters } = this.props;
+    const searchResults = (searchesByString[searchString] && searchesByString[searchString].searches) || [];
+    const isFetching = searchesByString[searchString] && searchesByString[searchString].isFetching;
+
+    const head = Home.getMeta();
+
+    return (
+      <div className="container text-center">
+        <Meta title={head.title} description={head.description} link={head.link} meta={head.meta} />
+
+        <Header />
+
+        <MyInfo email="alexander.sharma@gmail.com" name="Alexander Sharma" />
+
+        <Searchbox isFetching={isFetching} handleChange={this.handleChange} value={searchString} />
+
+        <Suggestions
+          isFetching={isFetching}
+          searchResults={searchResults}
+          films={films}
+          characters={characters}
+          searchString={searchString}
+        />
+
+      </div>
+    );
+  }
+}
+
+export default connect(state => ({
+  searchString: state.searchString,
+  films: state.films,
+  characters: state.characters,
+  searchesByString: state.searchesByString
+}), null)(Home);
---
+++ src/util/api-endpoints.js
@@ -0,0 +1,3 @@
+export const HOST_API = 'https://swapi.dev/api/';
+export const FILMS_ENDPOINT = 'https://swapi.dev/api/films';
+export const CHARACTERS_ENDPOINT = 'https://swapi.dev/api/people';
